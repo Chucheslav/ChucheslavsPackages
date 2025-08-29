1c7eb2e787c57e3e11fbee90e6a4f8e7a4e0ea1f
Chucheslav <chucheslav@mail.ru>
2025-08-29 22:47:51 +0300

    добавил UnityEvent invoker

---
+++ Assets/YarnExtensions/Runtime/YarnUnityEventInvoker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Yarn.Unity;
+
+namespace YarnExtensions
+{
+[AddComponentMenu("Custom Components/Yarn Spinner/UnityEvent Invoker")]
+public class YarnUnityEventInvoker : MonoBehaviour
+{
+    [SerializeField] private List<EventById> events = new ();
+    
+    [Serializable]
+    private class EventById
+    {
+        public string id;
+        public UnityEvent unityEvent;
+    }
+
+    [YarnCommand("invokeEvent")]
+    public void InvokeEvent(string id)
+    {
+        EventById ev = events.Find(e => e.id == id);
+        if (ev == null)
+        {
+            Debug.LogError($"Event with id {id} not found on object {name}");
+            return;
+        }
+
+        ev.unityEvent.Invoke();
+    }
+}
+}
