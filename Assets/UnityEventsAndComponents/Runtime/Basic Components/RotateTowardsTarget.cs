9da1ce7a6134573a10ffbdf2c743e884e9df1743
Chucheslav <chucheslav@mail.ru>
2025-09-22 14:59:18 +0300

    Добавлена возможность поворачиваться обратной стороной компонента RotateTowards Испралено множество мелких недочетов в документации

--- Assets/UnityEventsAndComponents/Runtime/Basic Components/RotateTowardsTarget.cs
+++ Assets/UnityEventsAndComponents/Runtime/Basic Components/RotateTowardsTarget.cs
@@ -7,11 +7,12 @@ public class RotateTowardsTarget : MonoBehaviour
 {
     public Transform target;
     [SerializeField] private Vector3 offset;
+    [SerializeField] private bool inverseDirection;
 
     private void Update()
     {
-        //transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
-        transform.LookAt(target.position + offset);
+        Vector3 direction = (transform.position - (target.position + offset)) * (inverseDirection ? 1 : -1);
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 }
 }
